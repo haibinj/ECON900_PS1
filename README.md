587e5d3d6425fb10a941f884d8e8f991bc818b98
Haibin <haibinj@clemson.edu>
2019-04-23 12:54:29 -0400

    add data_clean and cluster analysis

--- README.md
+++ README.md
@@ -12,14 +12,24 @@ The file named "game_parse.py" is used to parse the HTML files.
 BeautifulSoup is used to parse the file.
 Users need to change set the directory to save the parsed data. 
 The parsed data is saved in "csv" file.
+I ran the code on April 14, 2019. There was 1064 pages at that time. 
 
 3. board game data
-Some of the variable in the "csv" data contains some format that can not be run directly. I change the format in data process software.  I use Numbers on Mac machine.
+Some of the variable in the "csv" data contains some format that can not be run directly. I change the format in data process software. 
 I cleaned the data by deleting the variables that are not used in the machine learning part.
 The file named "boardgame_data.csv" is the data parsed from the parsing file.
-
+Since most of the games that do not have ranking number do not have list price neither, so I chop the data from the point that do not have a ranking number.
+The final data are in the ?csv? file named ?games_chop?.
 
 4. Analyzing the board game
 The file named "machine.py" is used to analyze the board game data.
 It looks at the relationship between the price of the game and the users rating of the game. 
-It uses the "GaussianMixture" to separate the games into three clusters. The graph "Gaussian_cluster" shows the result.
+It uses the "GaussianMixture" to separate the games into three clusters. 
+components = 2, silhouette_score =  0.39952376133117234 , Gaussian_cluster_graph_2 shows the plotting result.
+components = 3, silhouette_score =  0.44701307212489155 , Gaussian_cluster_graph_3 shows the plotting result.
+components = 4, silhouette_score =  0.5083493836471616 ,   Gaussian_cluster_graph_4 shows the plotting result.
+components = 5, silhouette_score =  0.36574401476811047 , Gaussian_cluster_graph_5 shows the plotting result.
+components = 6, silhouette_score =  0.2884292281841645 ,   Gaussian_cluster_graph_6 shows the plotting result.
+
+I also tried higher number of clusters, but the silhouette scores are lower.  It seem the games can be clustered into three or four groups.
+The price and the rating seems positively associated.
--- machine.py
+++ machine.py
@@ -5,92 +5,21 @@ from sklearn.mixture import GaussianMixture
 from sklearn import metrics
 
 
-dataset = pd.read_csv("dataset.csv",header = None)
+dataset = pd.read_csv("games_clean.csv",header = None)
+dataset = dataset.iloc[1:]
 print(dataset.head())
 print(dataset.dtypes)
-
-mean_rating = dataset[0].mean(axis = 0)
-print(mean_rating)
-dataset[0].fillna(mean_rating, inplace=True)
-
-mean_avg = dataset[1].mean(axis = 0)
-print(mean_avg)
-dataset[1].fillna(mean_avg, inplace=True)
-
-mean_price = dataset[2].mean(axis = 0)
-print(mean_price)
-dataset[2].fillna(mean_price, inplace=True)
-
-mean_number = dataset[3].mean(axis = 0)
-print(mean_number)
-dataset[3].fillna(mean_number, inplace=True)
-
-mean_year = dataset[4].mean(axis = 0)
-print(mean_year)
-dataset[4].fillna(mean_year, inplace=True)
 
 
 print(dataset.head)
 
 plt.scatter(dataset[0],dataset[2])
-plt.savefig("scatter.png")
-   #construct a machine and feed data to it.
-# kmeans_machine = KMeans(n_clusters = 2).fit(dataset)
+plt.savefig("graph_11.png")
 
-# kmeans_predictions = kmeans_machine.predict(dataset)
-# 		# kmeans_machine = KMeans(n_clusters = 3).fit_predict(dataset)
-# 		# this can combine the two steps into one.
-
-# 		# use metrics
-# kmeans_predictions3 = kmeans_machine.predict(dataset)
-
-# print(metrics.silhouette_score(dataset,kmeans_predictions3))
-
-# kmeans_machine4 = KMeans(n_clusters = 2).fit(dataset)
-
-# kmeans_predictions4 = kmeans_machine4.predict(dataset)
-
-# print(metrics.silhouette_score(dataset,kmeans_predictions4))
-# plt.scatter(dataset[3],dataset[2],c=kmeans_predictions4)
-# plt.savefig("scatter4.png")
-
-# for i in range(4):
-# 	n = i + 2
-# 	print(n)
-#    #construct a machine and feed data to it.
-# 	kmeans_machine = KMeans(n_clusters = n).fit(dataset)
-# 	kmeans_predict = kmeans_machine.predict(dataset)
-# 	print(metrics.silhouette_score(dataset,kmeans_predict))
-# 	plt.scatter(dataset[3],dataset[2],c=kmeans_predict)
-# 	plt.savefig("scatter"+str(n)+".png")
-
-	#====================================================
-# plt.scatter(dataset[3],dataset[4],c=kmeans_predictions)
-
-# plt.savefig("scatter_kmean3.png")
-gaussian_predicitons = GaussianMixture(n_components=2).fit(dataset).predict(dataset)
+gaussian_predicitons = GaussianMixture(n_components=8).fit(dataset).predict(dataset)
 print(metrics.silhouette_score(dataset,gaussian_predicitons))
-# 	# the number is smaller but not meaning that this is worse than the KMean cluster.
-# 	# the number is not comparable between different algorithms.
+
 
 plt.scatter(dataset[0],dataset[2],c=gaussian_predicitons)
 
-plt.savefig("scatter_gaussian.png")
-
-
-
-# gaussian_predicitons4 = GaussianMixture(n_components=4).fit(dataset).predict(dataset)
-# print(metrics.silhouette_score(dataset,gaussian_predicitons4))
-# plt.scatter(dataset[3],dataset[4],c=gaussian_predicitons4)
-
-# plt.savefig("scatter_gaussian4.png")
-
-# for i in range(4):
-# 	n = i + 2
-# 	print(n)
-#    #construct a machine and feed data to it.
-# 	gaussian_machine = GaussianMixture(n_components = n).fit(dataset)
-# 	gaussian_predict = gaussian_machine.predict(dataset)
-# 	print(metrics.silhouette_score(dataset,gaussian_predict))
-# 	plt.scatter(dataset[3],dataset[4],c=gaussian_predict)
-# 	plt.savefig("scatter"+str(n)+".png")
+plt.savefig("scatter_gaussian8.png")
